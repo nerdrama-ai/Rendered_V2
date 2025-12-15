c9fb9b2d573ce33df6de120fc3bc8c0abcb846d9
nerdrama.ai <nerdrama.ai@gmail.com>
2025-12-16 03:16:41 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import IsoWorld from "@/components/IsoWorld";
+
+export default function Home() {
+  return (
+    <main style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+      <IsoWorld />
+    </main>
+  );
+}
